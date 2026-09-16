4e4fd6d891378b53981128d5256229a47a97144e
Abhishek Kumar <157896639+Abhishekkuarj@users.noreply.github.com>
2026-09-16 08:41:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -62,7 +62,7 @@ OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'ankij247@ibl')
 # MongoDB Configuration
 # ============================
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Maik09hj:maikasr097877@cluster0.7nq9n31.mongodb.net/?appName=Cluster0")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://rohit826206_db_user:khan7860@cluster0.b1trxwz.mongodb.net/?appName=Cluster0")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://Govinda:fodP9j3crGP9RWkZ@cluster0.k9zgdxj.mongodb.net/?appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Maik09hj")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Deendayal_files')
 
